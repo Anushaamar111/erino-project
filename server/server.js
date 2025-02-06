ff464b6be290ccd5d191b72cbbdede0056e4b73a
Anushaamar111 <124653727+Anushaamar111@users.noreply.github.com>
2025-02-06 12:18:56 +0530

    change2

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 
+// Update allowedOrigins to include the frontend URL directly
 const allowedOrigins = [process.env.VITE_FRONTEND_URL, 'http://localhost:5173', 'https://erino-project-frontend.vercel.app'];
 
 app.use(cors({
@@ -29,6 +30,6 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/expenses", expenseRoutes);
 const PORT = process.env.PORT || 8000;
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
