4c422e025bf5684b9156d357c78862f2da9e12d5
Anushaamar111 <124653727+Anushaamar111@users.noreply.github.com>
2025-02-06 19:25:49 +0530

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -16,7 +16,21 @@ app.use(cookieParser());
 // Update allowedOrigins to include the frontend URL directly
 // const allowedOrigins = process.env.VITE_FRONTEND_URL;
 
-app.use(cors());
+const allowedOrigins = [process.env.VITE_FRONTEND_URL, "http://localhost:5173"];
+
+app.use(
+  cors({
+    credentials: true,
+    origin: function (origin, callback) {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/expenses", expenseRoutes);
